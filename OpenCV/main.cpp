844984bb8f2f1f75ce1295ac995069000c426004
Daniel Rembold <daniel.rembold@tuhh.de>
2018-05-09 23:20:20 +0200

    Added OpenCV example

---
+++ OpenCV/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2\opencv.hpp>
+
+using namespace cv;
+
+int main(void) {
+
+	Mat lena = imread("..\\lena.png");
+
+	if (lena.empty()) {
+		std::cerr << "Could not find image!" << std::endl;
+		std::cerr << "Make sure that the given path to the image is correct." << std::endl;
+	} else {
+		imshow("Playboy November 1972", lena);
+		waitKey();
+	}
+	
+	return 0;
+}
